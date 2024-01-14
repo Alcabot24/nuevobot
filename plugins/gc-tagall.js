b44e2ee215c6e1c250266e8596e4faadba9d11df
Alcabot <144275249+Alcabot24@users.noreply.github.com>
2024-01-14 16:49:45 -0600

    Actualizar gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,10 +5,10 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     throw false;
   }
   const pesan = args.join` `;
-  const oi = `*𝗠𝗘𝗡𝗦𝗔𝗝𝗘:* *𝗔𝗟𝗖𝗔𝗕𝗢𝗧|𝗡𝗘𝗪✨*`;
-  let teks = `*𝗠𝗘𝗡𝗖𝗜𝗢𝗡 𝗚𝗘𝗡𝗘𝗥𝗔𝗟*\n\n *𝗔𝗟𝗖𝗔𝗕𝗢𝗧|𝗡𝗘𝗪✨* \n\n🫰🏻 *𝗘𝗧𝗜𝗤𝗨𝗘𝗧𝗔𝗦:*\n`;
+  const oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* *𝘼𝙇𝘾𝘼²|𝘽𝙊𝙏*`;
+  let teks = `*𝙼𝙴𝙽𝙲𝙸𝙾𝙽 𝙶𝙴𝙽𝙴𝚁𝙰𝙻*\n\n *𝘼𝙇𝘾𝘼²|𝘽𝙊𝙏✨* \n\n🫰🏻 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`;
   for (const mem of participants) {
-    teks += `⚡️ @${mem.id.split('@')[0]}\n`;
+    teks += `🚨 @${mem.id.split('@')[0]}\n`;
   }
   conn.sendMessage(m.chat, {text: teks, mentions: participants.map((a) => a.id)} );
 };
